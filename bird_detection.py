0bda616857e32ecf08a7ddbdc4ae9c865865965a
bhavan <bhavanjaganath@gmail.com>
2026-09-01 23:44:35 +0530

    fixes

--- bird_detection.py
+++ bird_detection.py
@@ -1,27 +1,31 @@
-from ultralytics import YOLO
+# from ultralytics import YOLO
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 st.title("Real-Time Bird Detection App")
 
-@st.cache_resource
-def load_model():
-    return YOLO("best.pt")
+# @st.cache_resource
+# def load_model():
+#     return YOLO("best.pt")
 
-model = load_model()
+# model = load_model()
 
-class YOLOVideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        results = model(img, conf=0.6)
-        annotated_frame = results[0].plot()
-        return annotated_frame
+# class YOLOVideoTransformer(VideoTransformerBase):
+#     def transform(self, frame):
+#         img = frame.to_ndarray(format="bgr24")
+#         results = model(img, conf=0.6)
+#         annotated_frame = results[0].plot()
+#         return annotated_frame
     
 webrtc_streamer(
-    key="yolo-stream",
-    video_processor_factory=YOLOVideoTransformer,
+    key="yolo_stream",
     rtc_configuration={
         "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]},
-        ]}
+            {"urls": ["stun:stun.l.google.com:19302"]}
+        ]
+    },
+    media_stream_constraints={
+        "video": True,
+        "audio": False,
+    },
 )
