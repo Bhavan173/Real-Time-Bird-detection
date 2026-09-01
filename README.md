dfc923aff759d2911eacd85daccab0c90cb44ede
bhavan <bhavanjaganath@gmail.com>
2026-09-01 22:36:31 +0530

    Bird detection initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Real time Bird Detection for bird watching and photography
+- Made using YOLO Object detection
---
+++ bird_detection.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+import cv2
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+
+st.title("Real-Time Bird Detection App")
+
+@st.cache_resource
+def load_model():
+    return YOLO("best.pt")
+
+model = load_model()
+
+
+class YOLOVideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        
+        img = frame.to_ndarray(format="bgr24")
+
+        results = model(img, conf=0.6)
+        annotated_frame = results[0].plot()
+        return annotated_frame
+    
+webrtc_streamer(
+    key="yolo-stream",
+    video_processor_factory=YOLOVideoTransformer
+)
